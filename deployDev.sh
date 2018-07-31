35e6be5d93368e8aaa1a95e5493abdc54446cfad
Thompson <Lizzie.Thompson@thomsonreuters.com>
2018-07-31 15:42:33 +0100

    Beta Alias Test

--- deployDev.sh
+++ deployDev.sh
@@ -14,5 +14,6 @@ build_number=$1
 
 
 aws lambda create-alias --region ${REGION} --function-name ${NAME}Dev --description "dev" --function-version "\$LATEST" --name DEV
+aws lambda create-alias --region ${REGION} --function-name ${NAME}Dev --description "beta" --function-version 1 --name BETA
 
 aws lambda update-function-code --region ${REGION} --function-name ${NAME}Dev --zip-file fileb://Code/target/Launcher-${BUILD_VERSION}.jar
