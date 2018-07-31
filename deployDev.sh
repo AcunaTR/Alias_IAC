653f167a0e7ac52466be723f4740527759548e1d
Thompson <Lizzie.Thompson@thomsonreuters.com>
2018-07-31 12:10:46 +0100

    Create Alias Test

--- deployDev.sh
+++ deployDev.sh
@@ -10,12 +10,8 @@ CURDIR=`pwd`
 NAME=$(cat ./Code/lambdaName)
 VERSION=$(cat ./Code/version)
 BUILD_VERSION=$VERSION.$GO_PIPELINE_COUNTER
+build_number=$1
 
-aws lambda create-alias \
---region ${REGION} \
---function-name ${NAME} \
---description $build_number \
---function-version "\$LATEST"\
---name DEV \
+aws lambda create-alias --region ${REGION} --function-name ${NAME} --description $build_number --function-version ${VERSION} --name DEV
 
 aws lambda update-function-code --region ${REGION} --function-name ${NAME}Dev --zip-file fileb://Code/target/Launcher-${BUILD_VERSION}.jar
