c7157b237d5810ec73446762036244e36902da6d
Thompson <Lizzie.Thompson@thomsonreuters.com>
2018-07-31 14:14:42 +0100

    create alias test

--- deployDev.sh
+++ deployDev.sh
@@ -13,8 +13,8 @@ BUILD_VERSION=$VERSION.$GO_PIPELINE_COUNTER
 build_number=$1
 
 
-lambda_version=$(aws lambda list-versions-by-function --function-name ${NAME}Dev --region ${REGION} --output json|jq -r ".Versions[]| select(.Version!=\"\$LATEST\") | select(.Description == \"${build_number}\").Version")
+lambdaVersion=$(aws lambda list-versions-by-function --function-name ${NAME}Dev --region ${REGION} --output json|jq -r ".Versions[]| select(.Version!=\"\$LATEST\") | select(.Description == \"${build_number}\").Version")
 
-aws lambda create-alias --region ${REGION} --function-name ${NAME}Dev --description "dev" --function-version $lambda_version --name DEV
+aws lambda create-alias --region ${REGION} --function-name ${NAME}Dev --description "dev" --function-version $lambdaVersion --name DEV
 
 aws lambda update-function-code --region ${REGION} --function-name ${NAME}Dev --zip-file fileb://Code/target/Launcher-${BUILD_VERSION}.jar
