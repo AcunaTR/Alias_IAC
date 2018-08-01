18c3d1c51016f3a75ffd47000fad879763497983
Thompson <Lizzie.Thompson@thomsonreuters.com>
2018-08-01 09:11:38 +0100

    If loop implemented

--- deployDev.sh
+++ deployDev.sh
@@ -13,7 +13,12 @@ BUILD_VERSION=$VERSION.$GO_PIPELINE_COUNTER
 build_number=$1
 
 
-aws lambda create-alias --region ${REGION} --function-name ${NAME}Dev --description "dev" --function-version "\$LATEST" --name DEV
-aws lambda create-alias --region ${REGION} --function-name ${NAME}Dev --description "beta" --function-version 1 --name BETA
+if [[ $existing_aliases == *"\"$alias\""* ]]
+then
+   aws lambda update-alias --region ${REGION} --function-name ${NAME}Dev --description "dev" --function-version "\$LATEST" --name DEV
+else
+ aws lambda create-alias --region ${REGION} --function-name ${NAME}Dev --description "dev" --function-version "\$LATEST" --name DEV
+fi
+
 
 aws lambda update-function-code --region ${REGION} --function-name ${NAME}Dev --zip-file fileb://Code/target/Launcher-${BUILD_VERSION}.jar
