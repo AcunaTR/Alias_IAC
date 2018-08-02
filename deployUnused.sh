8546835e0b001ab0adfddb96f5961baafc5cf3fb
Thompson <Lizzie.Thompson@thomsonreuters.com>
2018-08-02 11:20:10 +0100

    unused alias assigned to most recent lambda

--- deployUnused.sh
+++ deployUnused.sh
@@ -13,11 +13,12 @@ BUILD_VERSION=$VERSION.$GO_PIPELINE_COUNTER
 build_number=$1
 
 existing_aliases=$(aws lambda list-aliases --function-name ${NAME}Dev --region ${REGION} --output json| jq -r '.Aliases[] | {Name: .Name}')
+lambda_version=$(aws lambda list-versions-by-function --function-name ${NAME}Dev --region ${REGION} --output json| jq -r ".Versions[] | select(.Version!=\"\$LATEST\") | select(.Description == \"${BUILD_VERSION}\").Version")
 
 if [[ $existing_aliases == *"UNUSED"* ]]; then
-  aws lambda update-alias --region ${REGION} --function-name ${NAME}Dev --description "unused" --function-version 7  --name UNUSED
+  aws lambda update-alias --region ${REGION} --function-name ${NAME}Dev --description "unused" --function-version ${lambda_version}  --name UNUSED
 else
-   aws lambda create-alias --region ${REGION} --function-name ${NAME}Dev --description "unused" --function-version 7 --name UNUSED
+   aws lambda create-alias --region ${REGION} --function-name ${NAME}Dev --description "unused" --function-version ${lambda_version} --name UNUSED
 fi
 
 OUTPUT=$(aws lambda get-alias --region ${REGION} --function-name ${NAME}Dev --name UNUSED) 
